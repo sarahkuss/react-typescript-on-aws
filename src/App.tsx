849d0b3ddd226ecb2a445c2af2c015e42168fdd4
Sarah Kuss <sarah.kuss33@gmail.com>
2023-05-04 16:00:09 -0400

    connected to api

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,22 @@ import { Form } from './components/Form';
 import './App.css';
 
 function App() {
+  const [serverData, setServerData] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('http://34.204.7.152:4000')
+    .then(res => res.json())
+    .then(data => setServerData(data))
+    .catch(err => console.error(err))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <Hello firstName='Sarah' lastName='Kuss' />
         <Form />
+        {/* @ts-ignore */}
+        {serverData && serverData.map(eachDog => <p>{eachDog.name}</p>)}
       </header>
     </div>
   );
