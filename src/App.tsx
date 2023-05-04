928c10133237627d26465fd16256aaf87c9a0ff9
Sarah Kuss <sarah.kuss33@gmail.com>
2023-05-04 13:42:45 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Hello} from './components/Hello'
+import { Form } from './components/Form';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hello firstName='Sarah' lastName='Kuss' />
+        <Form />
       </header>
     </div>
   );
